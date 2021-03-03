eebaac0c31029b789a528da8a92fdaa236d365b2
lordtemptation45 <67310702+lordtemptation45@users.noreply.github.com>
2021-03-04 00:08:17 +0700

    Update title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html>
 
 <head>
-    <title>Snapshot by TEMPLATED</title>
+    <title>clichepy</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="assets/css/main.css" />
